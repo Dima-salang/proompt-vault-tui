9af948da6c1f9477111a2305610927e2ae71d2e9
Dima-salang <luisgabrielle1026@gmail.com>
2025-12-21 01:18:03 +0800

    add: logger with log/slog

---
+++ log.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"os"
+	"log/slog"
+)
+
+func SetupLogger(debug bool) *slog.Logger {
+	level := slog.LevelInfo
+	if debug {
+		level = slog.LevelDebug
+	}
+	return slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: level}))
+}
